2e5459533bcd18b848fa5102c438cd4e735cea0c
Marc Görtz <mail@marcgoertz.de>
2025-08-01 19:07:11 +0200

    Fix linter issues.

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -2,4 +2,9 @@
 module.exports = {
 	customSyntax: 'postcss-styled-syntax',
 	extends: ['stylelint-config-standard'],
+	rules: {
+		// Disabled rules due to Styled Components
+		'no-invalid-position-declaration': null,
+		'nesting-selector-no-missing-scoping-root': null,
+	},
 };
